e6face1435e8548117d794958408a79b9020266d
Lukáš Machovec <lukasmachovecinfo@gmail.com>
2026-05-20 11:21:05 +0200

    commit

---
+++ 21-the-wild-oasis-website/the-wild-oasis-website/app/about/page.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import Navigation from "../components/Navigation";
+
+export default function Page() {
+    return (
+        <>
+            <h1>About</h1>
+        </>
+
+    )
+}
